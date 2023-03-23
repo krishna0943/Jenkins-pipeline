e5c61052bac2df839dbe0e1ffd3b6b17a8e06e1c
krishna0943 <59730233+krishna0943@users.noreply.github.com>
2023-03-23 18:54:38 -0400

    Update first.java

--- first.java
+++ first.java
@@ -1,6 +1,6 @@
 public class first{
     public static void main(String[] args)
     {
-        System.out.println("Hey this is Krishna and Suraj");
+        System.out.println("Hey this isKrishna and Suraj.This is our first jenkins pipeline");
     }
 }
